581616936771b7d1c1d6eae874ea5002ea198830
mrpejker <tatiana.yakushkina@gmail.com>
2023-11-20 21:02:09 +0000

    add page style is back

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -16,19 +16,15 @@ const DownloadPage: React.FC = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col">
-      {/* Campaign Element - Takes all available space */}
-      <div className="flex-grow p-10 text-center">
-        <Image
-          src="/Campaign.png"
-          alt="Campaign"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-
-      {/* Download Element - Positioned at the bottom */}
-      <div className="mb-10 rounded-lg bg-gray-200 p-10 text-center shadow-md">
+    <div
+      className="flex h-screen items-end justify-center"
+      style={{
+        backgroundImage: `url('/Campaign.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="mb-6 rounded-lg bg-gray-200 p-6 text-center shadow-md">
         <h1 className="mb-4 text-xl">Download Our App</h1>
 
         <a
